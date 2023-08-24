cc3a4d9585146c7ff4285d7cb60006a455706930
Emilse Arias <emilsearias@gmail.com>
2023-08-23 21:22:16 -0300

    agregamos un variable para guardar un puerto

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
-console.log('esta es una prueba dfsd2')
 // 1- configuraciones iniciales
+const app = express();
+// crear una variable
+app.set('port', process.env.PORT || 4000);
 
 // 2- middlewares
 
