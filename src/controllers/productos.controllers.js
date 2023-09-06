f5178a6363f82ee677b6047b170a7a3bbcd4ffc1
Emilse Arias <emilsearias@gmail.com>
2023-09-06 20:21:56 -0300

    agregamos el controlador para obtener un producto por el id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,66 +1,75 @@
-import Producto from "../models/producto.js"
+import Producto from "../models/producto.js";
 
-export const listarProductos = async (req, res) =>{
-    try{
-        // ir a la bd y pedir los productos
-       const productos = await Producto.find();
-       res.status(200).json(productos);
+export const listarProductos = async (req, res) => {
+  try {
+    // ir a la bd y pedir los productos
+    const productos = await Producto.find();
+    res.status(200).json(productos);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al buscar productos",
+    });
+  }
+};
 
-    }catch(error){
-        console.log(error)
-        res.status(404).json({
-            mensaje: "Error al buscar productos"
-        })
-    }
-}
+export const crearProducto = async (req, res) => {
+  try {
+    // ir a la bd y pedir los productos
+    // aqui los datos deberian estar validados
+    console.log(req.body);
+    const productoNuevo = new Producto(req.body);
+    //guardar el productoNuevo en la base de datos
+    await productoNuevo.save();
+    res.status(201).json({
+      mensaje: "El producto fue creado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "El producto no pudo ser creado",
+    });
+  }
+};
+export const editarProducto = async (req, res) => {
+  try {
+    // ir a la bd y pedir los productos
+    // aqui los datos deberian estar validados
+    //extraer el parametro id de la ruta
 
-export const crearProducto = async (req, res) =>{
-    try{
-        // ir a la bd y pedir los productos
-        // aqui los datos deberian estar validados
-        console.log(req.body)
-        const productoNuevo = new Producto(req.body);
-        //guardar el productoNuevo en la base de datos
-        await productoNuevo.save();
-        res.status(201).json({
-            mensaje: "El producto fue creado correctamente"
-        })   
-    }catch(error){
-        console.log(error);
-        res.status(400).json({
-            mensaje: "El producto no pudo ser creado"
-        })
-    }
-}
-export const editarProducto = async (req, res) =>{
-    try{
-        // ir a la bd y pedir los productos
-        // aqui los datos deberian estar validados
-        //extraer el parametro id de la ruta
-      
-        await Producto.findByIdAndUpdate(req.params.id,req.body);
-        res.status(200).json({
-            mensaje: "El producto fue modificado correctamente"
-        })   
-    }catch(error){
-        console.log(error);
-        res.status(400).json({
-            mensaje: "No se puede editar el producto"
-        })
-    }
-}
+    await Producto.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({
+      mensaje: "El producto fue modificado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "No se puede editar el producto",
+    });
+  }
+};
 
-export const borrarProducto = async (req, res) =>{
-    try{
-        await Producto.findByIdAndDelete(req.params.id);
-        res.status(200).json({
-            mensaje: "El producto fue eliminado correctamente"
-        })   
-    }catch(error){
-        console.log(error);
-        res.status(400).json({
-            mensaje: "No se puedo eliminar el producto"
-        })
-    }
-}
-
+export const borrarProducto = async (req, res) => {
+  try {
+    await Producto.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      mensaje: "El producto fue eliminado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "No se pudo eliminar el producto",
+    });
+  }
+};
+export const obtenerProducto = async (req, res) => {
+  try {
+    const productoBuscado = await Producto.findById(req.params.id);
+    res.status(200).json(productoBuscado);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "No se pudo obtener el producto",
+    });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { borrarProducto, crearProducto, editarProducto, listarProductos } from "../controllers/productos.controllers.js";
+import { borrarProducto, crearProducto, editarProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 router.route('/producto').get(listarProductos).post(crearProducto);
-router.route('/producto/:id').put(editarProducto).delete(borrarProducto);
+router.route('/producto/:id').put(editarProducto).delete(borrarProducto).get(obtenerProducto);
 // tarea agregar el modelo, ruta y controlador para agregar y listar usuarios
 export default router;
