2a4403a11ae2296a9254a1fbf9bbf269dc988c57
Emilse Arias <emilsearias@gmail.com>
2023-09-11 21:26:59 -0300

    agregamos un archivo estatico

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ import productoRouter from './src/routes/productos.routes.js';
 import usuarioRouter from './src/routes/usuario.routes.js';
 import './src/database/database.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // 1- configuraciones iniciales
 const app = express();
+console.log(fileURLToPath(import.meta.url));
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
 // crear una variable
 app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), ()=>{
@@ -22,9 +27,9 @@ app.use(express.urlencoded({extended:true})); // ayuda a interpretar datos del b
 app.use(morgan('dev')); //nos da mas informacion en la terminal
 // agregar un archivo estatico
 // console.log(path.join(__dirname ,'/public'));
-console.log(path.join('D:/RollingCode/22-23/c43i/05-backendCafecito' ,'/public'));
-app.use(express.static(path.join('D:/RollingCode/22-23/c43i/05-backendCafecito' ,'/public')))
-// app.use(express.static(new URL(import.meta.url).pathname + 'public'));
+// app.use(express.static(path.join('D:/RollingCode/22-23/c43i/05-backendCafecito' ,'/public')))
+app.use(express.static(path.join(_dirname, '/public')));
+
 // 3- crear las rutas
 // http://localhost:4000/api/producto
 app.use('/api',productoRouter)
