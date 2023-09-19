125467e4494bff2ea810e6843c540997ebd42f7c
Emilse Arias <emilsearias@gmail.com>
2023-09-18 22:45:22 -0300

    agregamos validaciones a los campos de imagen y precio

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -17,7 +17,28 @@ router
     [
       check("nombreProducto")
         .notEmpty()
-        .withMessage("El nombre de producto es un dato obligatorio"),
+        .withMessage("El nombre del producto es un dato obligatorio")
+        .isLength({ min: 2, max: 50 })
+        .withMessage(
+          "El nombre del producto debe tener entre 2 y 50 caracteres"
+        ),
+      check("precio")
+        .notEmpty()
+        .withMessage("El precio es un valor obligatorio")
+        .isNumeric()
+        .withMessage("El precio debe ser un número")
+        .custom((value) => {
+          if (value >= 1 && value <= 10000) {
+            return true;
+          } else {
+            throw new Error("El precio debe estar entre 1 y 10000");
+          }
+        }),
+        check('imagen')
+        .notEmpty()
+        .withMessage("La imagen es un valor obligatorio")
+        .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+        .withMessage("Debe ingresar una url de imagen valida")
     ],
     crearProducto
   );
